801ccfb3dda587af0d80dbd76e23255da31a25b8
DerFacn <nikitalupalo145@gmail.com>
2024-09-29 23:49:34 +0200

    Updated the start page
    Added a test view of the posts
    Added an icon to the basic template
    Added temporary images for display

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,7 +3,40 @@ from app.utils import jwt_required
 
 
 def index():
-    return render_template('index.html')
+    posts = [
+        {
+            "author_name": "Oleg Ivanenko",
+            "author_avatar": "/static/assets/images/avatar.png",
+            "author_url": "/static/assets/images/avatar.png",
+            "title": "How to grow kiwi.",
+            "description": "Short guide about growing kiwis at home.",
+            "image_url": "/static/assets/images/kiwi.png",
+            "url": "#",
+            "tags": ["Kiwi", "Growing", "Fruits"]
+        },
+        {
+            "author_name": "Kateryna Petrivna",
+            "author_avatar": "/static/assets/images/avatar.png",
+            "author_url": "/static/assets/images/avatar.png",
+            "title": "Useful properties of kiwi.",
+            "description": "Why kiwi is super-fruit?",
+            "image_url": "/static/assets/images/kiwi.png",
+            "url": "#",
+            "tags": ["Kiwi", "Health", "Vitamins"]
+        },
+        {
+            "author_name": "Dmitro Oleksi",
+            "author_avatar": "/static/assets/images/avatar.png",
+            "author_url": "/static/assets/images/avatar.png",
+            "title": "How to make kiwi-jam",
+            "description": "Recipe of kiwi jam.",
+            "image_url": "/static/assets/images/kiwi.png",
+            "url": "/posts/3",
+            "tags": ["Kiwi", "Recipies", "Jam"]
+        }
+    ]
+
+    return render_template('index.html', posts=posts)
 
 
 @jwt_required

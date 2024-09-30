c70accaf5c47223d20e31d8076e91da6cb7990d2
DerFacn <nikitalupalo145@gmail.com>
2024-09-30 12:37:05 +0200

    Errors handlers added

--- app/__init__.py
+++ app/__init__.py
@@ -11,14 +11,22 @@ def create_app(config_obj=Config):
     app = Flask(__name__)
     app.config.from_object(config_obj)
 
+    # Initialisation
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Models (for database)
     from . import models
 
+    # Registering all routes
     from .router import router
     app.register_blueprint(router)
 
+    # Registering all error handlers
+    from .errors import register_error_handlers
+    register_error_handlers(app)
+
+    # User lookup
     from .utils import get_user
 
     @app.before_request
---
+++ app/errors.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for
+
+
+def not_authorized_401(e):
+    # In case when user is not authorized - we're redirecting user to sign in page
+    return redirect(url_for('root.user.auth.login'))
+
+
+def register_error_handlers(app: Flask):
+    app.register_error_handler(401, not_authorized_401)
